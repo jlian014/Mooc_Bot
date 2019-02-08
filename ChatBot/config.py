5c99f339fd7fea87e2f8ab9e31396cf97350c630
jlian014 <32937965+jlian014@users.noreply.github.com>
2019-02-08 07:40:18 -0800

    Update config.py

--- ChatBot/config.py
+++ ChatBot/config.py
@@ -16,8 +16,8 @@ from slackclient import SlackClient
 ######## Slack configuration   ##########################
 ###################################################################
 
-SLACK_BOT_TOKEN='xoxb-522150663810-528835316439-tpB1awf6K72lCCmVjN28EiWr'
-SLACK_VERIFICATION_TOKEN='DQJpML1oPDMep1MkI6txDBeC' 
+SLACK_BOT_TOKEN=''
+SLACK_VERIFICATION_TOKEN='' 
 
 # instantiate Slack client
 slack_client = SlackClient(SLACK_BOT_TOKEN) # do not change this parameter
@@ -27,8 +27,8 @@ slack_client = SlackClient(SLACK_BOT_TOKEN) # do not change this parameter
 ###################################################################
 
 service = watson_developer_cloud.AssistantV1(
-    iam_apikey = '14oGuiHlj-84TIyuzRBfgOKpNWKYfQipWF2JxDmXSWjS', # replace with Password
+    iam_apikey = '', # replace with Password
     version = '2018-09-20'
 )
 
-workspace_id = 'd4e954b4-28ac-41bb-82f9-fee4c2aa7c5a' # replace with Assistant ID
+workspace_id = '' # replace with Assistant ID
